06bff124def7ba8c9659a73f47058e08cbc6e79d
Bhagi-Dhakal <Bhagirdhakal@gmail.com>
2026-06-13 21:56:31 -0400

    Matrix class improvement

--- Math/Matrix.hpp
+++ Math/Matrix.hpp
@@ -11,7 +11,6 @@
 #include <functional>
 #include <memory>
 #include <new>
-#include <ranges>
 #include <stdexcept>
 
 // Structs
@@ -38,6 +37,42 @@ class Matrix
           m_rows (rows),
           m_cols (cols) {};
 
+    // Copy Constructor
+    Matrix (Matrix const& m)
+        : Matrix (m.m_rows, m.m_cols)
+    {
+        std::copy (m.begin (), m.end (), this->begin ());
+    }
+
+    // Move Constructor
+    Matrix (Matrix&& other) noexcept
+        : m_data (std::move (other.m_data)),
+          m_rows (other.m_rows),
+          m_cols (other.m_cols)
+    {
+    }
+
+    // swap
+    void
+    swap (Matrix& other) noexcept
+    {
+    }
+
+    // Copy Assingment
+    Matrix&
+    operator= (Matrix m)
+    {
+        swap (m);
+        return *this;
+    }
+
+    Matrix&
+    operator= (Matrix&& m) noexcept
+    {
+        swap (m);
+        return *this;
+    }
+
     // Other methods
     unsigned
     rows () const
@@ -50,11 +85,20 @@ class Matrix
         return m_cols;
     };
 
+    // returns the shape of the matrix
+    // Rows X Cols
     Shape
-    size () const
+    order () const
     {
         return Shape { m_rows, m_cols };
     };
+
+    // return the number of elements in matrix
+    unsigned
+    size () const
+    {
+        return m_rows * m_cols;
+    }
 
     // returning the data
     iterator
@@ -72,16 +116,22 @@ class Matrix
     iterator
     end ()
     {
-        return this->begin () + this->rows () * this->cols ();
+        return this->begin () + this->size ();
     };
 
     const_iterator
     end () const
     {
-        return this->begin () + this->rows () * this->cols ();
+        return this->begin () + this->size ();
     };
 
     // return specefic elements
+
+    T&
+    operator[] (unsigned row, unsigned col)
+    {
+        return m_data[row * m_cols + col];
+    }
 
     T const&
     operator[] (unsigned row, unsigned col) const
@@ -94,7 +144,7 @@ class Matrix
     Matrix<T>
     operator+ (Matrix<T> const& B)
     {
-        if (!(m_rows == B.size ().rows && m_cols == B.size ().cols))
+        if (!(m_rows == B.rows () && m_cols == B.cols ()))
         {
             throw std::runtime_error ("Invalid Matrix for + operations. ");
         }
@@ -102,7 +152,7 @@ class Matrix
         Matrix<T> result (m_rows, m_cols);
 
         std::transform (
-          m_data.begin (), m_data.end (), result.begin (), std::plus<T> ());
+          begin (), end (), B.begin (), result.begin (), std::plus<> ());
 
         return result;
     }
@@ -112,7 +162,7 @@ class Matrix
     Matrix<T>
     operator- (Matrix<T> const& B)
     {
-        if (!(m_rows == B.size ().rows && m_cols == B.size ().cols))
+        if (!(m_rows == B.rows () && m_cols == B.cols ()))
         {
             throw std::runtime_error ("Invalid Matrix for - operations. ");
         }
@@ -120,7 +170,7 @@ class Matrix
         Matrix<T> result (m_rows, m_cols);
 
         std::transform (
-          m_data.begin (), m_data.end (), result.begin (), std::minus<T> ());
+          begin (), end (), B.begin (), result.begin (), std::minus<> ());
 
         return result;
     }
@@ -128,10 +178,16 @@ class Matrix
     // Multiply two Matrix with * operator
     // Returns: new result Matrix, This x B
     // Done using intrinsics, blocked for performance optimization
+
+    // Big Problem here
+    /*
+        TODO: only use blocking for big matrix not smaller ones
+        Fix multiply issue.
+    */
     Matrix<T>
     operator* (Matrix<T> const& B)
     {
-        if (!(m_rows == B.cols () && m_cols == B.rows ()))
+        if (!(m_cols == B.rows ()))
         {
             throw std::runtime_error ("Invalid Matrix for * operation. ");
         }
@@ -142,14 +198,26 @@ class Matrix
         unsigned A_order = m_cols * m_rows;
         unsigned B_order = B.cols () * B.rows ();
 
-        // Getting messy
         for (unsigned k = 0; k < A_order; k += BLOCK_SIZE)
         {
             for (unsigned i = 0; i < A_order; i += BLOCK_SIZE)
             {
                 for (unsigned j = 0; j < A_order; j += BLOCK_SIZE)
                 {
-                    /// Same for Blocks + intrinsics
+                    // Same for Blocks + intrinsics
+                    for (unsigned k1 = k; k1 < k + BLOCK_SIZE; ++k1)
+                    {
+                        for (unsigned i1 = i; i1 < i + BLOCK_SIZE; ++i1)
+                        {
+
+                            T r = m_data[i * k];
+
+                            for (unsigned j1 = j; j1 < j + BLOCK_SIZE; ++j1)
+                            {
+                                result[i, j] += r * B[k, i];
+                            }
+                        }
+                    }
                 }
             }
         }
--- main.cpp
+++ main.cpp
@@ -16,5 +16,18 @@ main ()
     Matrix<int> A (2, 2);
     std::iota (A.begin (), A.end (), 1);
 
-    std::println ("{}", A);
+    Matrix<int> B (2, 2);
+    std::iota (B.begin (), B.end (), 1);
+
+    std::println ("A:\n{}", A);
+    std::println ("B:\n{}", B);
+
+    Matrix<int> C = A + B;
+    std::println ("C:\n{}", C);
+
+    Matrix<int> D = A - B;
+    std::println ("D:\n{}", D);
+
+    Matrix<int> E = A * B;
+    std::println ("E:\n{}", E);
 }
